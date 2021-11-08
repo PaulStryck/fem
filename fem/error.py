ac348b1ea2194bd9080048503ee13fceb7c08b21
Stryck, Paul <paul.stryck@studium.uni-hamburg.de>
2021-11-08 14:39:06 +0100

    Error computation clarified

--- fem/error.py
+++ fem/error.py
@@ -5,27 +5,43 @@ from fem.integration import gauss_legendre_quadrature
 
 def error(u, v, f):
     '''
-    :param v: must be a FEFunction
+    Compute ∫ f(u(x), v(x)) dx on Ω
+    :param v: must be a :class FEFunction: with domain Ω
     :param u: python function, range of u and v must be of the same dimension
-    Integral of f(u(x), v(x)) on Ω
-        Where Ω is the domain of v
     :param f: python function taking two arguments mapping to 1D
     '''
+
+    # Quadrature rule of order 36 where the spation dimension
+    # matches the spation dimension of the reference element of the mesh
     quadrature = gauss_legendre_quadrature(v.fs.element.cell.dim,
                                            36)
 
+    # Evaluate the local basis function at all quadrature points
     phi = v.fs.element.phi_eval(quadrature.points)
+
+    # Store the intermediate values and compute sum afterwords to prevent
+    # round off errors
     integral = []
+
+    # Outer sum. Splitting Ω into mesh elements
     for e, nodes in v.fs.elements:
-        f_i     = v.fs.element.cell.affine_transform(e)
+        # Mapping for reference elemnt to current mesh element
+        F_i     = v.fs.element.cell.affine_transform(e)
+
         detj, _ = v.fs.element.cell.affine_transform_jacobian(e)
 
-        u_f_i_x = np.array(list(
-            map(u, map(f_i, quadrature.points))
+        # u(F(x)) for all quadrature points
+        u_F_i_x = np.array(list(
+            map(u, map(F_i, quadrature.points))
         ))
 
-        v_f_i_x = np.dot(v.coefficients[nodes], phi.T)
-        f_x = np.array(list(map(f, u_f_i_x, v_f_i_x)))
+        # v(F(x)) for all quadrature points
+        v_F_i_x = np.dot(v.coefficients[nodes], phi.T)
+
+        # f(u(F(x)), v(F(x)))
+        f_x = np.array(list(map(f, u_F_i_x, v_F_i_x)))
+
+        # finally quadrature weights and jacobian determinant
         val = np.dot(f_x, quadrature.weights) * detj
         integral.append(val)
 
