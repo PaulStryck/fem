ed028d03e2bef9d7aa13f2ee82f5c5df0a1e8c10
Stryck, Paul <paul.stryck@studium.uni-hamburg.de>
2021-11-10 00:18:32 +0100

    experimental parallelisation

--- pdes/poisson.py
+++ pdes/poisson.py
@@ -1,3 +1,9 @@
+import ctypes as c
+import multiprocessing as mp
+import timeit
+from contextlib import closing
+from functools import reduce
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.sparse import bmat, coo_matrix
@@ -11,19 +17,89 @@ from fem.integration import gauss_legendre_quadrature
 from fem.mesh import SimplexMesh
 
 
-def asm(fs, f):
+def mp_init(
+    s_quad_ws_,
+    s_dPhi_,
+    s_Phi_,
+    cell_,
+    shapes
+):
+    global s_quad_ws, s_dPhi, s_Phi, cell
+    s_quad_ws = np.frombuffer(s_quad_ws_.get_obj()).reshape(shapes[0])
+    s_dPhi    = np.frombuffer(s_dPhi_.get_obj()).reshape(shapes[1])
+    s_Phi     = np.frombuffer(s_Phi_.get_obj()).reshape(shapes[2])
+    cell      = cell_
+
+def mp_integrate(x):
+    e, ind = x
+
+    detj, jTinv = cell.affine_transform_jacobian(e)
+
+    # transform each gradient with jTinv
+    G = s_dPhi @ jTinv.T
+
+
+    # compute local stiffness
+    localStiffness = np.tensordot(s_quad_ws,
+                                  G@G.swapaxes(1,2),
+                                  axes=1) * detj
+
+    # compute local mass
+    localMass = s_Phi.T @ (np.multiply(s_quad_ws, s_Phi.T).T) * detj
+
+    l = localMass.shape[0]*localMass.shape[1]
+    n = 0
+    data    = np.empty((2,l), dtype=np.double)
+    indices = np.empty((2,l), dtype=np.uint)
+
+    for k,l in np.ndindex(*localStiffness.shape):
+        indices[0,n] = ind[k]
+        indices[1,n] = ind[l]
+
+        data[0,n] = localStiffness[k,l]
+        data[1,n] = localMass[k,l]
+
+        n += 1
+
+    return indices, data
+
+def mp_asm(quadrature, dPhi, Phi, cell, es):
+    s_quad_ws = mp.Array(c.c_double, quadrature.weights.flatten())
+    s_dPhi    = mp.Array(c.c_double, dPhi.flatten())
+    s_Phi     = mp.Array(c.c_double, Phi.flatten())
+
+    shapes    = [quadrature.weights.shape,
+                 dPhi.shape,
+                 Phi.shape]
+    initargs = (s_quad_ws, s_dPhi, s_Phi, cell, shapes,)
+
+    with closing(mp.Pool(initializer=mp_init, initargs=initargs)) as p:
+        parts = p.map(mp_integrate, es)
+        l = len(parts[0][0][0])
+        indices = np.empty((2,len(parts)*l), dtype=np.uint)
+        data    = np.empty((2,len(parts)*l))
+
+        s1 = timeit.default_timer()
+        for i in range(len(parts)):
+            indices[0][i*l:(i+1)*l] = parts[i][0][0]
+            indices[1][i*l:(i+1)*l] = parts[i][0][1]
+            data[0][i*l:(i+1)*l] = parts[i][1][0]
+            data[1][i*l:(i+1)*l] = parts[i][1][1]
+
+    mass      = coo_matrix((data[1], (indices[0], indices[1])))
+    stiffness = coo_matrix((data[0], (indices[0], indices[1])))
+    s2 = timeit.default_timer()
+    print("copying: {}".format(s2-s1))
+
+    return stiffness, mass
+
+
+
+def asm(fs, f, multiprocessing=True):
     '''
     Asseble system matrix of weak formulation for
     -∆u = f
     '''
-    n = 0
-    numDataPts = fs.mesh.entities_per_dimension[-1] * fs.element.dim**2
-    _i = np.empty(numDataPts, dtype=int)
-    _j = np.empty(numDataPts, dtype=int)
-
-    _data_s = np.empty(numDataPts, dtype=np.double)
-    _data_m = np.empty(numDataPts, dtype=np.double)
-
     quadrature = gauss_legendre_quadrature(fs.element.cell.dim, fs.element.dim)
 
     # dPhi \in \R^(q,n,p), where:
@@ -34,40 +110,53 @@ def asm(fs, f):
     dPhi = fs.element.grad_phi_eval(quadrature.points)
     Phi  = fs.element.phi_eval(quadrature.points)
 
-    for e, ind in fs.elements:
-        # e, ind = (x_1, ..., x_n), (i(x_1), ... i(x_n))
-        detj, jTinv = fs.element.cell.affine_transform_jacobian(e)
+    if multiprocessing:
+        stiffness, mass = mp_asm(quadrature, dPhi, Phi, fs.element.cell,
+                                 fs.elements)
+    else:
+        n = 0
+        numDataPts = fs.mesh.entities_per_dimension[-1] * fs.element.dim**2
+        _i = np.empty(numDataPts, dtype=int)
+        _j = np.empty(numDataPts, dtype=int)
 
-        # transform each gradient with jTinv
-        G = dPhi @ jTinv.T
+        _data_s = np.empty(numDataPts, dtype=np.double)
+        _data_m = np.empty(numDataPts, dtype=np.double)
 
-        # compute local stiffness
-        localStiffness = np.tensordot(quadrature.weights,
-                                      G@G.swapaxes(1,2),
-                                      axes=1) * detj
+        for e, ind in fs.elements:
+            # e, ind = (x_1, ..., x_n), (i(x_1), ... i(x_n))
+            detj, jTinv = fs.element.cell.affine_transform_jacobian(e)
 
-        # compute local mass
-        localMass = Phi.T @ (np.multiply(quadrature.weights, Phi.T).T) * detj
+            # transform each gradient with jTinv
+            G = dPhi @ jTinv.T
 
-        # map localStiffness to global stifness
-        for k,l in np.ndindex(*localStiffness.shape):
-            _i[n] = ind[k]
-            _j[n] = ind[l]
+            # compute local stiffness
+            localStiffness = np.tensordot(quadrature.weights,
+                                          G@G.swapaxes(1,2),
+                                          axes=1) * detj
 
-            _data_s[n] = localStiffness[k,l]
-            _data_m[n] = localMass[k,l]
-            n += 1
+            # compute local mass
+            localMass = Phi.T @ (np.multiply(quadrature.weights, Phi.T).T) * detj
+
+            # map localStiffness to global stifness
+            for k,l in np.ndindex(*localStiffness.shape):
+                _i[n] = ind[k]
+                _j[n] = ind[l]
+
+                _data_s[n] = localStiffness[k,l]
+                _data_m[n] = localMass[k,l]
+                n += 1
+
+        mass      = coo_matrix((_data_m, (_i, _j)))
+        stiffness = coo_matrix((_data_s, (_i, _j)))
 
     # compute projection of RHS
     _f = FEFunction(fs)
     _f.interpolate(f)
 
-    mass      = coo_matrix((_data_m, (_i, _j)))
-    stiffness = coo_matrix((_data_s, (_i, _j)))
     return stiffness, mass@_f.coefficients
 
 
-def poisson(m: SimplexMesh, f, g, order=1):
+def poisson(m: SimplexMesh, f, g, order=1, multiprocessing=True):
     # Create Finite Dim function space
     fe = PElement(order, m.element)
     fs = FEFunctionSpace(m, fe)
@@ -83,8 +172,10 @@ def poisson(m: SimplexMesh, f, g, order=1):
     # FEFunctionSpace should be aware of BCs?!
     # FEFunctionSpace is part of the co-chain complex, thus should exhibit a
     # boundary operator
-
-    A, b = asm(fs, f)
+    s1 = timeit.default_timer()
+    A, b = asm(fs, f, multiprocessing)
+    s2 = timeit.default_timer()
+    print("asm: {}".format(s2-s1))
 
     # TODO: non-zero Neumann BC must be taken into account here
     n = fs.dim
@@ -130,16 +221,20 @@ if __name__ == '__main__':
               2: ([],([],[])),
               3: ([],([],[]))}
 
-    for d in [1,2,3]:
-        for n in range(2,31,2):
+    for d in [3]:
+        for n in [100]:
             # m = SimplexMesh.Create_2d_unit_square_unstructured(n)
             m = SimplexMesh.Create_2d_unit_square_structured(n)
             # m = SimplexMesh.Create_1d_unit_interval_structured(n)
             # m = SimplexMesh.Create_2d_manifold(n)
             u = poisson(m, f, g, d)
+            u = poisson(m, f, g, d, False)
+            print()
 
-            e_l2 = error(s, u, lambda x,y: np.abs(x-y)**2)
-            e_h1 = error(gs, u, lambda x,y: np.dot(x-y,x-y), grad=True)
+            # e_l2 = error(s, u, lambda x,y: np.abs(x-y)**2)
+            # e_h1 = error(gs, u, lambda x,y: np.dot(x-y,x-y), grad=True)
+            e_l2 = 0
+            e_h1 = 0
 
             print("n: {}, L2 error = {}".format(n, e_l2**.5))
             print("n: {}, H1 error = {}".format(n, e_h1**.5))
