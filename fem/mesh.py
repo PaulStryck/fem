c2e9ba841287005ee83389f04adc63a2f1742a22
Stryck, Paul <paul.stryck@studium.uni-hamburg.de>
2021-11-13 22:40:56 +0100

    import gmsh

--- fem/mesh.py
+++ fem/mesh.py
@@ -418,3 +418,115 @@ class SubSimplexMesh(SimplexMesh):
         m_2 = SubSimplexMesh(outer=self._outer,
                              cells=self.nfaces[d_sub].halfdeepcopy(ma_2))
         return m_1, m_2
+
+
+def import_gmsh(file: str):
+    tags = [dict(), dict(), dict(), dict()]
+
+    f = open(file, 'r')
+    it = iter(f.readlines())
+
+    while(next(it).strip() != '$MeshFormat'): pass
+    v = next(it).strip().split(' ')
+
+    # only v4.1 supported
+    assert float(v[0]) == 4.1
+
+
+    while(next(it).strip() != '$Entities'): pass
+    t = next(it).strip().split(' ')
+    numTags = [int(t[0]), int(t[1]), int(t[2]), int(t[3])]
+
+
+    # all point tags
+    for i in range(numTags[0]):
+        l = next(it).strip().split(' ')
+        if(int(l[3]) != 0):
+            tags[0][int(l[0])] = int(v[4])
+
+    # all multi dimensional tags
+    for i in [1,2,3]:
+        for j in range(numTags[i]):
+            l = next(it).strip().split(' ')
+            if(int(l[7]) != 0):
+               tags[i][int(l[0])] = int(l[8])
+
+    # skip to nodes
+    while(next(it).strip() != '$Nodes'): pass
+
+    l = next(it).strip().split(' ')
+    blocks = int(l[0])
+    nnodes = int(l[1])
+
+    nodes = []
+    nodeNumbering = dict()
+
+    n = 0
+
+    for i in range(blocks):
+        l = next(it).strip().split(' ')
+        nodesInBlock = int(l[3])
+
+        # The node numbers
+        for j in range(nodesInBlock):
+            l = next(it).strip().split(' ')
+            nodeNumbering[int(l[0])] = n
+            n += 1
+
+        # The actual coordinates
+        for j in range(nodesInBlock):
+            l = next(it).strip().split(' ')
+            nodes.append((float(l[0]), float(l[1])))
+
+    # skip to elements
+    while(next(it).strip() != '$Elements'): pass
+
+    l = next(it).strip().split(' ')
+    blocks = int(l[0])
+
+    edges = set()
+    triangles = set()
+
+    for i in range(blocks):
+        l = next(it).strip().split(' ')
+        elemDim = int(l[0])
+        elemEntity = int(l[1])
+        elemType = int(l[2])
+        elemsInBlock = int(l[3])
+
+        for j in range(elemsInBlock):
+            l = next(it).strip().split(' ')
+
+            if elemType == 1:
+                edges.add( (0, int(l[1]), int(l[2])) )
+                # edges.add( (tags[elemDim][elemEntity], int(l[1]), int(l[2])) )
+            elif elemType == 2:
+                triangles.add((0, int(l[1]),
+                               int(l[2]), int(l[3])))
+                # triangles.add((tags[elemDim][elemEntity], int(l[1]),
+                #                int(l[2]), int(l[3])))
+            else:
+                print("Unsupported Element Type: {}".format(elemType))
+                continue
+
+    renumbering = dict()
+    vertices = []
+    faces = []
+    d_boundaries = []
+    n_boundaries = []
+
+    for t in triangles:
+        _t = (nodeNumbering[t[1]], nodeNumbering[t[2]],nodeNumbering[t[3]])
+
+        for k in _t:
+            if k not in renumbering:
+                vertices.append(nodes[k])
+                renumbering[k] = len(vertices)-1
+
+        faces.append((renumbering[_t[0]], renumbering[_t[1]], renumbering[_t[2]]))
+
+    for e in edges:
+        d_boundaries.append(renumbering[nodeNumbering[e[1]]])
+        d_boundaries.append(renumbering[nodeNumbering[e[2]]])
+
+    return SimplexMesh(vertices, faces, referenceTriangle)
