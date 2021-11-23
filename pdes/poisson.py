80b8db557ceee4f3c416ff47fe3e488e70edbaaa
Stryck, Paul <paul.stryck@studium.uni-hamburg.de>
2021-11-23 21:27:44 +0100

    integration of rhs without projection

--- pdes/poisson.py
+++ pdes/poisson.py
@@ -1,10 +1,8 @@
-from typing import Callable, Optional, Tuple
+from typing import Callable, Tuple
 
 import numpy as np
-import numpy.typing as npt
 import sympy
 from scipy.sparse import bmat
-from scipy.sparse.coo import coo_matrix
 from scipy.sparse.linalg.dsolve.linsolve import spsolve
 
 from fem.boundary_conditions import (asm_dirichlet_boundary,
@@ -20,16 +18,40 @@ BoundaryCondition = Tuple[Callable, FEFunctionSpace]
 
 
 def poisson(fs: FEFunctionSpace,
-            f: FEFunction,
+            fx: Callable,
             db: BoundaryCondition,
             nb: BoundaryCondition = None):
-    L, M = fs.asm_stiff_mass()
+    L, _ = fs.asm_stiff_mass()
 
     R, _, db_repr = asm_dirichlet_boundary(db, fs.dim)
 
     nb_repr = asm_neumann_boundary(nb) if nb is not None else np.zeros(fs.dim)
 
-    f_int = M@f.coefficients
+    ######## TEST ##########
+    numDataPts = fs.dim
+
+    f_int = np.zeros(numDataPts, dtype=np.double)
+    quadrature = gauss_legendre_quadrature(fs.element.cell.dim,
+                                           fs.element.dim**2)
+    Phi  = fs.element.phi_eval(quadrature.points)
+
+    for e, ind in fs.elements:
+        detj, _ = fs.element.cell.affine_transform_jacobian(e)
+        F_i = fs.element.cell.affine_transform(e)
+
+        fxs = np.array([fx(F_i(x)) for x in quadrature.points])
+
+        loc_f_int = np.sum(
+            np.multiply(np.multiply(fxs, quadrature.weights), Phi.T).T * detj,
+            axis = 0
+        )
+
+        for k in range(len(loc_f_int)):
+            f_int[ind[k]] += loc_f_int[k]
+
+    ######## END TEST ##########
+
+    # f_int = M@f.coefficients
     sys = bmat([[L, R], [R.T, None]], format='csr')
 
     rhs = np.concatenate((f_int + nb_repr, db_repr))
@@ -76,75 +98,152 @@ if __name__ == '__main__':
 
     # s = lambda x: x[0]**a
 
-    a = 50
-    xc = np.array([-0.05,-0.05])
-    r0 = 0.7
-    x, y = sympy.symbols('x y')
-    expr = sympy.atan(a*(sympy.sqrt((x-xc[0])**2+(y-xc[1])**2) - r0))
+    # a = 50
+    # xc = np.array([-0.05,-0.05])
+    # r0 = 0.7
+    # x, y = sympy.symbols('x y')
+    # expr = sympy.atan(a*(sympy.sqrt((x-xc[0])**2+(y-xc[1])**2) - r0))
 
-    dxx = sympy.diff(expr, x, x)
-    dyy = sympy.diff(expr, y, y)
+    # dxx = sympy.diff(expr, x, x)
+    # dyy = sympy.diff(expr, y, y)
 
-    f_dxx = sympy.lambdify((x,y), dxx)
-    f_dyy = sympy.lambdify((x,y), dyy)
-
-    def f(_x):
-        x = _x[0]
-        y = _x[1]
-        fx = f_dxx(x,y)
-        fy = f_dyy(x,y)
-
-        if np.isnan(fx):
-            fx = 0
-
-        if np.isnan(fy):
-            fy = 0
-
-        return -(fx+fy)
+    # f_dxx = sympy.lambdify((x,y), dxx)
+    # f_dyy = sympy.lambdify((x,y), dyy)
 
     # def f(_x):
     #     x = _x[0]
     #     y = _x[1]
-    #     r = np.linalg.norm(_x-xc)
-    #     g = a*(r-r0)
-    #     gx = a*(x-xc[0])/r
-    #     gxx = (a*r - a*(x-xc[0])**2)/(r**3)
-    #     gy = a*(y-xc[1])/r
-    #     gyy = (a*r - a*(y-xc[1])**2)/(r**3)
+    #     fx = f_dxx(x,y)
+    #     fy = f_dyy(x,y)
 
-    #     sxx = (gxx*(g**2+1) - 2*g*(gx**2)) / ((gxx**2+1)**2)
-    #     syy = (gyy*(g**2+1) - 2*g*(gy**2)) / ((gyy**2+1)**2)
-    #     return sxx+syy
+    #     if np.isnan(fx):
+    #         fx = 0
 
-    s = lambda x: np.arctan(a * (np.linalg.norm(x - xc)-r0))
+    #     if np.isnan(fy):
+    #         fy = 0
+
+    #     return -(fx+fy)
+
+    # s = lambda x: np.arctan(a * (np.linalg.norm(x - xc)-r0))
+    # a = 2./3.
+    # x, y = sympy.symbols('x y')
+
+    # expr_r = (sympy.sqrt(x**2+y**2)**a)*sympy.sin(a*sympy.atan(y/x))
+    # expr_l = (sympy.sqrt(x**2+y**2)**a)*sympy.sin(a*(sympy.atan(y/x)+np.pi))
+
+    # dxx_r = sympy.diff(expr_r, x, x)
+    # dxx_l = sympy.diff(expr_l, x, x)
+    # dyy_r = sympy.diff(expr_r, y, y)
+    # dyy_l = sympy.diff(expr_l, y, y)
+
+    # f_dxx_r = sympy.lambdify((x,y), dxx_r)
+    # f_dxx_l = sympy.lambdify((x,y), dxx_l)
+    # f_dyy_r = sympy.lambdify((x,y), dyy_r)
+    # f_dyy_l = sympy.lambdify((x,y), dyy_l)
+
+    # def s(_x):
+    #     x = _x[0]
+    #     y = _x[1]
+    #     r = np.linalg.norm(_x)
+    #     if x == 0 and y == 0: t = 0
+    #     elif x == 0 and y > 0: t = np.pi/2.
+    #     elif x == 0 and y < 0: t = 3*np.pi/2.
+    #     elif x < 0: t = np.arctan(y/x) + np.pi
+    #     else: t = np.arctan(y/x)
+
+    #     return r**a*np.sin(a*t)
+
+    # def f(_x):
+    #     x = _x[0]
+    #     y = _x[1]
+    #     if x >= 0:
+    #         fx = f_dxx_r(x,y)
+    #         fy = f_dyy_r(x,y)
+    #     else:
+    #         fx = f_dxx_l(x,y)
+    #         fy = f_dyy_l(x,y)
+
+    #     if np.isnan(fx):
+    #         fx = 0
+
+    #     if np.isnan(fy):
+    #         fy = 0
+
+    #     return -(fx+fy)
+
+    a = 1.1
+    b = 0.
+
+    x, y = sympy.symbols('x y')
+    s_1_e = sympy.cos(np.pi * y / 2.)
+    s_2_e = sympy.cos(np.pi * y / 2.) + (x - b * (y+1))**a
+    # s_1_e = 0. * x
+    # s_2_e = x**a
+
+    s_1xx = sympy.diff(s_1_e, x, x)
+    s_1yy = sympy.diff(s_1_e, y, y)
+
+    s_2xx = sympy.diff(s_2_e, x, x)
+    s_2yy = sympy.diff(s_2_e, y, y)
+
+    l_1 = sympy.lambdify((x,y), (s_1xx + s_1yy))
+    l_2 = sympy.lambdify((x,y), (s_2xx + s_2yy))
+    s_1 = sympy.lambdify((x,y), s_1_e)
+    s_2 = sympy.lambdify((x,y), s_2_e)
+
+    def s(_x):
+        x = _x[0]
+        y = _x[1]
+        fx = s_1(x,y) if x <= b*(y+1) else s_2(x,y)
+
+        if np.isnan(fx):
+            print("nan: {}, {}".format(x,y))
+        return fx if not (np.isnan(fx) or np.isinf(fx)) else 0.
+
+    def f(_x):
+        x = _x[0]
+        y = _x[1]
+        if x < b*(y+1): fx = l_1(x,y)
+        else: fx = l_2(x,y)
+
+        if np.isnan(fx):
+            print("nan: {}, {}".format(x,y))
+
+        return -fx if not (np.isnan(fx) or np.isinf(fx)) else 0.
 
 
-    ns = range(5, 65+1, 5)
+    ns = range(12, 200+1, 12)
     ns = [20]
-    d = 3
-    # for n in range(2,30,2):
+    d = 1
+    # d = 3
+    errors = []
     for n in ns:
         m = SimplexMesh.Create_2d_unit_square_structured(n)
+        # m = SimplexMesh.Create_2d_refined(n)
+        # m = SimplexMesh.Create_2d_unit_square_unstructured(n)
+        # m = SimplexMesh.Create_2d_L(10, (2.5/2.)*np.pi)
+        # m = import_gmsh('./lshape.msh')
 
         fs = FEFunctionSpace(m, PElement(d, m.element))
         f_interp = FEFunction(fs)
-        f_interp.interpolate(f)
+        # f_interp.interpolate(f)
 
         db_fs = FEFunctionSpace(m.boundary_mesh,
                                 PElement(d, m.element.lower_element))
 
         u = poisson(
             fs,
-            f_interp,
+            f,
             (s, db_fs)
         )
 
         e_l2 = error(s, u, lambda x,y: np.abs(x-y)**2)**0.5
         print(e_l2)
+        # errors.append(e_l2)
         # plot_mesh(m, True)
 
-        sol = FEFunction(fs)
-        sol.interpolate(f)
-        sol.plot(1)
-        # u.plot(1)
-
+        # sol = FEFunction(fs)
+        # sol.interpolate(f)
+        # sol.plot(1)
+        u.plot(1)
+    print(errors)
